b0a1e352943276844102d2667d77990a2db6c06e
zarlashta manana <zarlashta27@yahoo.com>
2018-08-28 17:18:41 -0400

    Added the model file for the fantasy table

--- db/seeds.sql
+++ db/seeds.sql
@@ -17,18 +17,18 @@ VALUE ("Hamster", "Mesocricetus", "4", "7.5", "135.60", "Adorable", false,'/imag
 -- ===========================================================================================================================
 
 -- FANTASY PETS CATEGORY======================================================================================================
-INSERT INTO fantasy_animals (name, age, size, temperament, price, from_where, matching_real_animal, adopted, pic)  
-VALUES ("Smaug", 171, 82, "arrogant", 5,000,000,000,000.50, "Middle Earth: Lonely Mountain", "lizard", false,'/images/Smaug.jpg');
+INSERT INTO fantasy_animals (name, age, size_in_feet, temperament, price, from_where, matching_real_animal, adopted, pic)  
+VALUES ("Smaug", "171", "82", "arrogant", "5000.50", "Middle Earth: Lonely Mountain", "lizard", false,'/images/Smaug.jpg');
 
-INSERT INTO fantasy_animals (name, age, size, temperament, price, from_where, matching_real_animal, adopted, pic,)  
-VALUES ("Pikachu", 17, 1, "endearing", 2,530.00, "Pokemon World: Kanto", "chinchilla", false,'/images/Pikachu.jpg');
+INSERT INTO fantasy_animals (name, age, size_in_feet, temperament, price, from_where, matching_real_animal, adopted, pic)  
+VALUES ("Pikachu","17", "1", "endearing", "2530.00", "Pokemon World: Kanto", "chinchilla", false,'/images/Pikachu.jpg');
 
-INSERT INTO fantasy_animals (name, age, size, temperament, price, from_where, matching_real_animal, adopted, pic)  
-VALUES ("Eevee", 5, 0.5, "adorable", 5,000.00, "Pokemon World: Kanto", "fox", true,'/images/Eevee.jpg');
+INSERT INTO fantasy_animals (name, age, size_in_feet, temperament, price, from_where, matching_real_animal, adopted, pic)  
+VALUES ("Eevee", "5", "0.5", "adorable", "5000.00", "Pokemon World: Kanto", "fox", true,'/images/Eevee.jpg');
 
-INSERT INTO fantasy_animals (name, age, size, temperament, price, from_where, matching_real_animal, adopted, pic)  
-VALUES ("Hedwig", 6, 1, "loyal", 450.00, "Harry Potter: Hogwarts", "owl", true,'/images/hedwig.jpg');
+INSERT INTO fantasy_animals (name, age, size_in_feet, temperament, price, from_where, matching_real_animal, adopted, pic)  
+VALUES ("Hedwig", "6", "1", "loyal", "450.00", "Harry Potter: Hogwarts", "owl", true,'/images/hedwig.jpg');
 
-INSERT INTO fantasy_animals (name, age, size, temperament, price, from_where, matching_real_animal, adopted, pic)  
-VALUES ("Buckbeak", 4, 6, "arrogant", 10,000.00, "Harry Potter: Hogwarts", "horse", false,'/images/buckbeak.jpg');
--- ===============================================================================================================
+INSERT INTO fantasy_animals (name, age, size_in_feet, temperament, price, from_where, matching_real_animal, adopted, pic)  
+VALUES ("Buckbeak", "4", "6", "arrogant", "10000.00", "Harry Potter: Hogwarts", "horse", false,'/images/buckbeak.jpg');
+-- ======================"=========================================================================================

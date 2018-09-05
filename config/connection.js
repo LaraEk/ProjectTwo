1421633b3b15876bbadd43569db9f8c1af5d091f
Ekscursionist <larakek@gmail.com>
2018-09-05 11:32:14 -0400

    creating files for contactform and about page to fill in with ranock's html

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@
 var Sequelize = require("sequelize");
 
 // Creates mySQL connection using Sequelize, first argument is database name, the empty string in the third argument spot is our password.
-var sequelize = new Sequelize("pets_db", "root", "Ahmad_2001", {
+var sequelize = new Sequelize("pets_db", "root", "password", {
   host: "localhost",
   port: 3306,
   dialect: "mysql",

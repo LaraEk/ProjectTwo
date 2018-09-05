65dfa0ece5ca8662f82c54d33f8196e336c50fb0
PersephoneUnderground <persephoneunderground@users.noreply.github.com>
2018-09-04 20:24:26 -0400

    attempted testing of adoption-req.js before establishing post api route but ran into issues with default form behavior. Going to establish api route first before further testing.

--- public/js/adoption-req.js
+++ public/js/adoption-req.js
@@ -1,22 +1,44 @@
 //This file pulls adoption request form data and sends POST request from client to server-side
 
-//TODO- in js connected to index, set up onclick event for "adopt" button to show form and send data-id from the submit button to the "data-pet-id" attribute and the pet name to the title
+//TODO- set up onclick event for "adopt" button to show form and send data-id from the submit button to the "data-pet-id" attribute and the pet name to the title
+    //code for form popup here
+
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function() {
+// $(function() {
 //get form inputs when submitted
-$("#adopt-req-form").on("submit", function(event) {
-    event.preventDefault();
+// $("#adopt-req-form").on("submit", function(event) {
+//     event.preventDefault();
 
-    // grab the form elements and save them in key:value pairs within an object
-    var newAdoptReq = {
-        clientName: $("#client-name").val().trim(),
-        clientEmail: $("#client-email").val().trim(),
-        clientPhone: $("#client-phone").val().trim(),
-        clientMessage: $("#client-message").val().trim(),
-        petId: $("#submit-adopt-form").data("pet-id")
-    };
+    
 
-    console.log(newAdoptReq);
+$(document).ready(function () {
+    $("#adopt-req-form").submit(function(event) {
+        console.log("DEBUGGING - form preventDefault about to run");
+        event.preventDefault(); // Prevent the form from submitting via the browser
+        console.log("DEBUGGING - form preventDefault just ran");
+        // grab the form elements and save them in key:value pairs within an object
+        var newAdoptReq = {
+            clientName: $("#client-name").val().trim(),
+            clientEmail: $("#client-email").val().trim(),
+            clientPhone: $("#client-phone").val().trim(),
+            clientMessage: $("#client-message").val().trim(),
+            petId: $(this).data("pet-id")
+        };
+
+        console.log(newAdoptReq);
+        console.log("DEBUGGING - about to run AJAX");
+
+        $.ajax({
+            type: $(this).attr('method'),
+            url: "/api/adopt-form",
+            data: newAdoptReq
+        }).done(function(data) {
+            // Optionally alert the user of success here...
+        }).fail(function(data) {
+            // Optionally alert the user of an error here...
+        });
+    });
+});
 
     // var ajaxUrl = "/api/adopt-form"
 
@@ -37,28 +59,28 @@ $("#adopt-req-form").on("submit", function(event) {
     //     );
 
     // export the object- send POST request to api routes file
-    $.post("/api/adopt-form", newAdoptReq,
-        function(data) { //receives the result of the post function
-            if(data) {
-                // if a response is returned, meaning the POST was successful
-                console.log(data);//console log response for testing purposes
+    // $.post("/api/adopt-form", newAdoptReq,
+    //     function(data) { //receives the result of the post function
+    //         if(data) {
+    //             // if a response is returned, meaning the POST was successful
+    //             console.log(data);//console log response for testing purposes
                 
-                // var confirmDiv = $("#confirmDiv");
-                // confirmDiv.text("Thank you! Your adoption request has been received. We'll get back to you within 3 business days. If you have any questions, please email us directly at adoptions@exampledomain.com .");
+    //             // var confirmDiv = $("#confirmDiv");
+    //             // confirmDiv.text("Thank you! Your adoption request has been received. We'll get back to you within 3 business days. If you have any questions, please email us directly at adoptions@exampledomain.com .");
 
-                alert("Thank you! Your adoption request has been received. We'll get back to you within 3 business days. If you have any questions, please email us directly at adoptions@exampledomain.com .");
+    //             alert("Thank you! Your adoption request has been received. We'll get back to you within 3 business days. If you have any questions, please email us directly at adoptions@exampledomain.com .");
 
-            }
-           else {
-            //if there is an error and no response is returned
+    //         }
+    //        else {
+    //         //if there is an error and no response is returned
 
-                // var confirmDiv = $("#confirmDiv");
-                // confirmDiv.text("We're sorry, an error prevented submission of your request. Please email us directly at adoptions@exampledomain.com .");
+    //             // var confirmDiv = $("#confirmDiv");
+    //             // confirmDiv.text("We're sorry, an error prevented submission of your request. Please email us directly at adoptions@exampledomain.com .");
 
-                alert("We're sorry, an error prevented submission of your request. Please email us directly at adoptions@exampledomain.com .");
-           }
+    //             alert("We're sorry, an error prevented submission of your request. Please email us directly at adoptions@exampledomain.com .");
+    //        }
             
-    });
+    // });
 
     //Clear the form when submitting- 
     //TODO- add more efficient method, clear form command
@@ -68,7 +90,7 @@ $("#adopt-req-form").on("submit", function(event) {
     // $("#client-phone").val("");
     // $("#client-message").val("");
     
-});
-});
+// });
+// });
 
 //send ajax POST request using form data in request body
